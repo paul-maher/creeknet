5752a36155f0c815f52cd388b22e0625cdac67f8
Paul Maher <36451678+paul-maher@users.noreply.github.com>
2018-11-30 11:30:42 +0000

    Change data format
    remove the appended milliseconds in the trace files

--- creektracer.py
+++ creektracer.py
@@ -15,7 +15,7 @@ class CreekTracer:
         #handler.setLevel(logging.INFO)
 
         # create a logging format
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt = '%Y-%m-%d %H:%M:%S')
         handler.setFormatter(formatter)
 
         # add the handlers to the logger
